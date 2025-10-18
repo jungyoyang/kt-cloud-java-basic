988e3e9b72659ea58f8b7d91269bfdff9e6b3fa7
Jungyo Yang(Window) <2jungyo@naver.com>
2025-10-18 22:51:28 +0900

    카페 역할정리 클래스생성 및 static을 왜붙여야할까

---
+++ src/main/java/com/kt/techup/cafe/Cafe_OOP.java
@@ -0,0 +1,31 @@
+package com.kt.techup.cafe;
+
+//역할, 책임, 협력
+
+//역할 정리
+// 카페 - 주문 흐름 제어
+// 캐셔 - 주문받기, 음료 제공
+// 바리스타 - 음료 제조
+// 고객 - 주문, 수령
+
+public class Cafe_OOP {
+	// 고객
+	//todo 왜 static을 붙여야할까?
+	static class Customer{
+	}
+
+	static class Cashier{
+
+	}
+
+	static class Barista{
+
+	}
+
+	static class Cafe{
+
+	}
+
+
+
+}
