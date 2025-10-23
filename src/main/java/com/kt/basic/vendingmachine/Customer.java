f659e9ceb7d66d961611219f4960bfb1c4491eda
Jungyo Yang(Window) <2jungyo@naver.com>
2025-10-24 00:54:52 +0900

    자판기 2차 커밋

--- src/main/java/com/kt/basic/vendingmachine/Customer.java
+++ src/main/java/com/kt/basic/vendingmachine/Customer.java
@@ -1,11 +1,28 @@
 package com.kt.basic.vendingmachine;
 
+import java.util.Scanner;
+
 public class Customer {
 	Customer(){
 
 	}
 
-	moneyin
+	void infrontofmachine(){
+
+	}
+
+
+
+	void putmoneyin(int money){
+		Scanner sc = new Scanner(System.in);
+		money = sc.nextInt();
+		System.out.println(money +"를 넣으셨습니다");
+		System.out.println("현재 " +money);
+	}
+	void select(int a){
+
+	}
+
 
 
 
---
+++ src/main/java/com/kt/basic/vendingmachine/VendingMachineMain.java
@@ -0,0 +1,44 @@
+package com.kt.basic.vendingmachine;
+
+public class VendingMachineMain {
+
+	public static void main(String[] args) {
+
+		//관리자가 자판기앞에선다
+		Manager manager1 = new Manager();
+		VendingMachine machine1 = new VendingMachine();
+
+		//관리자가 자판기를 켠다
+		manager1.turnon();
+
+		//사람이 자판기 앞에선다
+		Customer customer1 = new Customer();
+		customer1.infrontofmachine();
+
+		//자판기 화면 on
+		machine1.vendingview();
+
+		// 돈을 넣어
+		customer1.putmoneyin();
+		// 버튼을 눌러
+		customer1.select();
+
+
+		// 돈이 모자르면 "돈이 부족합니다" 출력
+		// 돈이 충분하면 음료수를 주고 잔돈을 출력
+		machine1.calculate();
+
+
+	}
+
+
+
+	// 관리자가 앞에서서 -99를 입력하면
+	// 자판기의 매출을 출력하도록하고
+	// 관리자가 회수를 입력하면 매출이 초기화되고
+	// 관리자의 장부에 해당 날짜와 매출액이 기록되도록한다.
+
+	// 관리자가 보고를 입력 하면 장부에 해당하는 내용들이 쭉 출력되도록한다.
+
+	// 자판기의 온오프버튼이 있어서 이건 관리자만 조작가능하다.
+}
